47a3f06d01149723287ca3bfe041dcb2e38ac25e
heweidong7 <511650494@qq.com>
2024-09-18 13:59:03 +0800

    first commit

---
+++ preprocess.py
@@ -0,0 +1,320 @@
+# -*- coding: utf-8 -*-  
+"""
+@Author: winton
+@File: preprocess.py
+@Time: 2018/5/17 下午9:24
+@Description:
+"""
+import json
+import pickle
+import collections
+
+import pandas as pd
+import numpy as np
+# import pymongo
+# from sklearn import preprocessing
+from tqdm import tqdm
+
+import setting
+
+
+def one_hot(index, depth):
+    temp = [0] * depth
+    temp[index] = 1
+    return temp
+
+
+def get_logistic_data(path,
+                      field,
+                      train_start_year,
+                      train_end_year,
+                      test_start_year,
+                      test_end_year):
+    data = pd.read_csv(path, sep=',', header=0)
+    data['year_copy'] = data['year']
+    data = pd.get_dummies(data[field + ['DV_ICW', 'year_copy']], columns=['year', 'Industry'])
+    train_data = data[(data.year_copy >= train_start_year) & (data.year_copy <= train_end_year)]
+    test_data = data[(data.year_copy >= test_start_year) & (data.year_copy <= test_end_year)]
+    x_field = list(train_data.columns)
+    x_field.remove('DV_ICW')
+    x_field.remove('year_copy')
+    train_x = train_data[x_field]
+    train_y = train_data['DV_ICW']
+    test_x = test_data[x_field]
+    test_y = test_data['DV_ICW']
+    return train_x, train_y, test_x, test_y
+
+
+def get_crf_data(path, field, end_year, test=False):
+    data = pd.read_csv(path, sep=',', header=0)
+    data = pd.get_dummies(data[field + ['DV_ICW', 'stkcd']], columns=['Industry'])
+    data_group = data.groupby('stkcd')
+    x_field = list(data.columns)
+    x_field.remove('DV_ICW')
+    x_field.remove('year')
+    x_field.remove('stkcd')
+    print(len(x_field))
+    if test:
+        max_len = end_year - 2006 + 1
+    else:
+        max_len = end_year - 2006 + 2
+    x = []
+    y = []
+    seq_len = []
+    for name, value in data_group:
+        if test and value[value.year == end_year].empty:
+            continue
+        temp = value[value.year <= end_year]
+        if not temp.empty:
+            temp_x = np.array(temp[x_field])
+            temp_y = np.array(temp['DV_ICW'])
+            len_ = temp_x.shape[0]
+            zeros = np.zeros((max_len - len_, temp_x.shape[1]))
+            temp_x = np.concatenate((temp_x, zeros), axis=0)
+            zero = np.zeros(max_len - len_)
+            temp_y = np.concatenate((temp_y, zero), axis=0)
+            x.append(temp_x)
+            y.append(temp_y)
+            seq_len.append(len_)
+    return np.array(x, dtype=np.float32), np.array(y, dtype=np.int32), np.array(seq_len, dtype=np.int32)
+
+
+def get_svm(path,
+            field,
+            train_start_year,
+            train_end_year,
+            test_start_year,
+            test_end_year):
+    data = pd.read_csv(path, sep=',', header=0)
+    data['year_copy'] = data['year']
+    data = pd.get_dummies(data[field + ['DV_ICW', 'year_copy', 'stkcd']], columns=['year', 'Industry'])
+    train_data = data[(data.year_copy >= train_start_year) & (data.year_copy <= train_end_year)]
+    test_data = data[(data.year_copy >= test_start_year) & (data.year_copy <= test_end_year)]
+    x_field = list(train_data.columns)
+    x_field.remove('DV_ICW')
+    x_field.remove('year_copy')
+    x_field.remove('stkcd')
+    train_f = train_data[x_field]
+    train_l = train_data['DV_ICW']
+    train_g = train_data['stkcd']
+    test_f = test_data[x_field]
+    test_l = test_data['DV_ICW']
+    test_g = test_data['stkcd']
+    return np.array(train_f), train_g, train_l, np.array(test_f), test_g, test_l
+
+
+def get_paiwise_year():
+    ids = {2006: {1: [], 0: []},
+           2007: {1: [], 0: []},
+           2008: {1: [], 0: []},
+           2009: {1: [], 0: []},
+           2010: {1: [], 0: []},
+           2011: {1: [], 0: []},
+           2012: {1: [], 0: []},
+           2013: {1: [], 0: []},
+           2014: {1: [], 0: []},
+           2015: {1: [], 0: []},
+           }
+    with pymongo.MongoClient(setting.HOST) as conn:
+        conn['admin'].authenticate(setting.USER, setting.PASSWORD)
+        db = conn['quexian']
+        coll = db['feature1']
+        coll2 = db['feature1_pair_year']
+        for content in coll.find():
+            ids[content['year']][content['label']].append(content['_id'])
+        for year in ids:
+            for pos in ids[year][1]:
+                for neg in ids[year][0]:
+                    coll2.insert({
+                        'year': year,
+                        'pos_id': pos,
+                        'neg_id': neg
+                    })
+
+
+def get_paiwise_stkcd(year):
+    ids = collections.defaultdict(dict)
+    with pymongo.MongoClient(setting.HOST) as conn:
+        conn['admin'].authenticate(setting.USER, setting.PASSWORD)
+        db = conn['quexian']
+        coll = db['feature1']
+        coll2 = db['feature1_pair_stkcd_{}'.format(year)]
+        for content in coll.find({'year': {'$lt': year}}):
+            if content['label'] not in ids[content['stkcd']]:
+                ids[content['stkcd']][content['label']] = [content['_id']]
+            else:
+                ids[content['stkcd']][content['label']].append(content['_id'])
+        for stkcd in ids:
+            if 1 in ids[stkcd] and 0 in ids[stkcd]:
+                for pos in ids[stkcd][1]:
+                    for neg in ids[stkcd][0]:
+                        coll2.insert({
+                            'stkcd': stkcd,
+                            'pos_id': pos,
+                            'neg_id': neg
+                        })
+
+
+def get_padding_data(path):
+    data = pd.read_csv(path, sep=',', header=0)
+    # del data['Unnamed: 0']
+    data_group = data.groupby('stkcd')
+    x_field = list(data.columns)
+    x_field.remove('DV_ICW')
+    x_field.remove('year')
+    x_field.remove('stkcd')
+    print(len(x_field))
+    min_max_scaler = preprocessing.MinMaxScaler()
+    min_max_scaler.fit(data[x_field])
+    with pymongo.MongoClient(setting.HOST) as conn:
+        conn['admin'].authenticate(setting.USER, setting.PASSWORD)
+        db = conn['quexian']
+        coll = db['feature1_padding_5_norm']
+        coll.remove()
+        for name, value in tqdm(data_group):
+            temp_data = []
+            for year in range(2006, 2016):
+                temp = value[value.year <= year]
+                if len(temp) > 1:
+                    temp = temp.sort_values(['year'], ascending=1)
+                temp_x = min_max_scaler.transform(temp[x_field])
+                temp_y = int(temp[temp.year == year]['DV_ICW'])
+                temp_y_2 = np.array(temp[temp.year <= year]['DV_ICW'])
+                seq_len = temp_x.shape[0]
+                temp_data.append({
+                    'stkcd': str(int(name)),
+                    'year': year,
+                    'seq_len': seq_len,
+                    'feature': pickle.dumps(temp_x),
+                    'label': temp_y,
+                    'label2': pickle.dumps(temp_y_2)
+                })
+            coll.insert_many(temp_data)
+
+
+def get_rnn_data(path, field):
+    data = pd.read_csv(path, sep=',', header=0)
+    data = pd.get_dummies(data[field + ['DV_ICW', 'stkcd']], columns=['Industry'])
+    data_group = data.groupby('stkcd')
+    x_field = list(data.columns)
+    x_field.remove('DV_ICW')
+    x_field.remove('year')
+    x_field.remove('stkcd')
+    print(len(x_field))
+    # min_max_scaler = preprocessing.MinMaxScaler()
+    # min_max_scaler.fit(data[x_field])
+    with pymongo.MongoClient(setting.HOST) as conn:
+        db = conn['quexian']
+        coll = db['feature2_new']
+        coll.remove()
+        for name, value in tqdm(data_group):
+            temp_data = []
+            for year in range(2006, 2017):
+                temp = value[value.year <= year]
+                test = value[value.year == year]
+                if not test.empty:
+                    # temp_x = min_max_scaler.transform(temp[x_field])
+                    temp_x = np.array(temp[x_field])
+                    temp_y = int(temp[temp.year == year]['DV_ICW'])
+                    temp_y_2 = np.array(temp[temp.year <= year]['DV_ICW'])
+                    len_ = year - 2005 - temp_x.shape[0]
+                    # padding
+                    if len_ > 0:
+                        temp_x = np.concatenate((np.zeros((len_, temp_x.shape[1])), temp_x))
+                        temp_y_2 = np.concatenate((np.zeros(len_), temp_y_2))
+                    seq_len = temp_x.shape[0]
+                    temp_data.append({
+                        'stkcd': str(name),
+                        'year': year,
+                        'seq_len': seq_len,
+                        'feature': pickle.dumps(temp_x),
+                        'label': temp_y,
+                        'label2': pickle.dumps(temp_y_2)
+                    })
+            if temp_data:
+                coll.insert_many(temp_data)
+
+
+def get_rnn_data2(path, field):
+    data = pd.read_csv(path, sep=',', header=0)
+    data = pd.get_dummies(data[field + ['DV_ICW', 'stkcd']], columns=['Industry'])
+    data_group = data.groupby('stkcd')
+    print(list(data.columns))
+    x_field = list(data.columns)
+    x_field.remove('DV_ICW')
+    x_field.remove('year')
+    x_field.remove('stkcd')
+    with pymongo.MongoClient(setting.HOST) as conn:
+        db = conn['quexian']
+        coll = db['feature2']
+        coll.remove()
+        for name, data_frame in tqdm(data_group):
+            temp_data = {'stkcd': str(name), 'data': {}}
+            for index, item in data_frame.iterrows():
+                year = item['year']
+                label = item['DV_ICW']
+                feature = item[x_field]
+                temp_data['data'].update({
+                    str(int(year)): {
+                        'label': label,
+                        'feature': list(feature)
+                    }
+                })
+            coll.insert_one(temp_data)
+
+
+def get_rnn_json_data2(path, field, out_path):
+    data = pd.read_csv(path, sep=',', header=0)
+    data = pd.get_dummies(data[field + ['DV_ICW', 'stkcd']], columns=['Industry'])
+    data_group = data.groupby('stkcd')
+    print(list(data.columns))
+    x_field = list(data.columns)
+    x_field.remove('DV_ICW')
+    x_field.remove('year')
+    x_field.remove('stkcd')
+    res = []
+    for name, data_frame in tqdm(data_group):
+        temp_data = {'stkcd': str(name), 'data': {}}
+        for index, item in data_frame.iterrows():
+            year = item['year']
+            label = item['DV_ICW']
+            feature = item[x_field]
+            temp_data['data'].update({
+                str(int(year)): {
+                    'label': label,
+                    'feature': list(feature)
+                }
+            })
+        res.append(temp_data)
+    with open(out_path, 'w') as f:
+        json.dump(res, f)
+
+
+if __name__ == '__main__':
+    # get_rnn_data('data/ICD_dataset_180716.csv', field=setting.FEATURE2_NEW)
+    # get_rnn_data2('data/ICD_dataset_180716.csv', field=setting.FEATURE1_NEW)
+    # for end_year in [2011, 2012, 2013, 2014]:
+    #     a, b, c, d, e, f = get_svm('data/ICD_dataset_180515.csv',
+    #                                setting.FEATURE1,
+    #                                2006,
+    #                             pre   end_year,
+    #                                end_year + 1,
+    #                                end_year + 1)
+    #     with open('data/{}.train.svm'.format(end_year + 1), 'w') as f1:
+    #         for i, j, k in zip(a, b, c):
+    #             f1.write('{} qid:{} '.format(int(k), int(j)))
+    #             for index, value in enumerate(i):
+    #                 f1.write('{}:{} '.format(index + 1, value))
+    #             f1.write('#\n')
+    #     with open('data/{}.test.svm'.format(end_year + 1), 'w') as f2:
+    #         for i, j, k in zip(d, e, f):
+    #             f2.write('{} qid:{} '.format(int(k), int(j)))
+    #             for index, value in enumerate(i):
+    #                 f2.write('{}:{} '.format(index + 1, value))
+    #             f2.write('#\n')
+    # get_paiwise_year()
+    # get_paiwise_stkcd(2012)
+    # get_padding_data('data/feature1_padding_5.csv')
+    # get_rnn_json_data2('data/ICD_dataset_180716.csv', field=setting.FEATURE1_NEW, out_path='data/rnn_feature1_new.json')
+    # get_rnn_json_data2('data/ICD_dataset_180716.csv', field=setting.FEATURE2_NEW, out_path='data/rnn_feature2_new.json')
+    get_rnn_json_data2('data/ICD_dataset_240218.csv', field=setting.FEATURE1_NEW, out_path='data/rnn_feature1_new_240218.json')
---
+++ rnn_with_json_data.py
@@ -0,0 +1,325 @@
+# -*- coding: utf-8 -*-  
+"""
+@Author: winton
+@File: rnn_with_json_data.py
+@Time: 2024/2/26 15:14
+@Description:
+"""
+import json
+import os
+
+import tensorflow as tf
+import numpy as np
+import photinia as ph
+
+from evaluation import evaluation
+
+# hyper parameter
+tf.flags.DEFINE_string('gpu', '0', 'Choose which GPU to use.')
+tf.flags.DEFINE_string('input_file', 'data/rnn_feature1_new_240218.json', 'Input json file.')
+tf.flags.DEFINE_string('unit', 'gru', 'Type of RNN unit.')
+tf.flags.DEFINE_integer('emb', 200, 'Embedding size.')
+tf.flags.DEFINE_integer('hidden', 300, 'Hidden layer size.')
+tf.flags.DEFINE_integer('layers', 2, 'RNN layers.')
+tf.flags.DEFINE_integer('batch', 256, 'Batch size.')
+tf.flags.DEFINE_integer('loops', 1000, 'Num loops.')
+tf.flags.DEFINE_integer('start', 2006, 'Train start year.')
+tf.flags.DEFINE_integer('end', 2011, 'Train end year.')
+tf.flags.DEFINE_float('threshold', 0.3, 'Threshold.')
+tf.flags.DEFINE_float('keep', 0.9, 'Keep prob.')
+tf.flags.DEFINE_bool('bi', True, 'If use bidirectional RNN.')
+
+FLAGS = tf.flags.FLAGS
+DATA_TYPE = tf.float32
+
+
+class JSONData(object):
+    def __init__(self,
+                 file,
+                 start_year,
+                 end_year,
+                 feature=None
+                 ):
+        self._train_stkcd = []
+        self._train_year = []
+        self._train_x = []
+        self._train_y = []
+        self._train_len = []
+        self._test_x = []
+        self._test_y = []
+        self._test_len = []
+        self._test_stkcd = []
+        self._feature = feature
+        train_max_len = end_year - start_year + 1
+        test_max_len = end_year - start_year + 2
+        end_year = end_year
+        test_year = end_year + 1
+        with open(file, 'r') as f:
+            data = json.load(f)
+        for doc in data:
+            temp = list(sorted(doc['data'].items(), key=lambda d: int(d[0])))  # sort by year
+            temp_x, temp_y, temp_len, temp_year = self._get_data(temp, start_year, end_year, train_max_len)
+            if temp_len > 0:
+                self._train_stkcd.append(doc['stkcd'])
+                self._train_year.append(temp_year)
+                self._train_x.append(temp_x)
+                self._train_y.append(temp_y)
+                self._train_len.append(temp_len)
+            if str(test_year) in doc['data']:
+                self._test_stkcd.append(doc['stkcd'])
+                temp_x, temp_y, temp_len, _ = self._get_data(temp, start_year, test_year, test_max_len)
+                self._test_x.append(temp_x)
+                self._test_y.append(temp_y)
+                self._test_len.append(temp_len)
+        self._data_set = ph.Dataset(self._train_x, self._train_y, self._train_len)
+
+    def next_batch(self, size=0):
+        return self._data_set.next_batch(size)
+
+    def _get_data(self, data, start, end, max_len):
+        x = []
+        y = []
+        years = []
+        for key, value in data:
+            if start <= int(key) <= end:
+                years.append(int(key))
+                if self._feature is not None:
+                    temp = np.array(value['feature'])
+                    x.append(temp[self._feature])
+                else:
+                    x.append(value['feature'])
+                y.append([value['label']])
+        len_ = len(y)
+        if len_ > 0:
+            feature_size = len(x[0])
+            for i in range(max_len - len_):
+                x.append([0] * feature_size)
+                y.append([0])
+        return x, y, len_, years
+
+    @property
+    def test_data(self):
+        return self._test_x, self._test_y, self._test_len
+
+    @property
+    def test_stkcd(self):
+        return self._test_stkcd
+
+    @property
+    def train_size(self):
+        return self._data_set.size
+
+    @property
+    def train_data(self):
+        return self._train_x, self._train_y, self._train_len
+
+    @property
+    def train_stkcd(self):
+        return self._train_stkcd
+
+    @property
+    def train_year(self):
+        return self._train_year
+
+
+class Model(object):
+    def __init__(self, is_training, input_size, output_size):
+        self._input = tf.placeholder(name='input', dtype=DATA_TYPE, shape=(None, None, input_size))
+        self._seq_len = tf.placeholder(name='seq_len', dtype=tf.int32, shape=(None,))
+        self._true_output = tf.placeholder(name='true_output', dtype=DATA_TYPE, shape=(None, None, output_size))
+        embedding = self.linear_layer('emb', self._input, input_size, FLAGS.emb, axes=[[2], [0]])
+        # embedding = tf.nn.leaky_relu(embedding, alpha=0.2)
+        # dropout for input
+        if is_training and FLAGS.keep < 1:
+            embedding = tf.nn.dropout(embedding, FLAGS.keep)
+        # cell
+        if FLAGS.unit == 'lstm':
+            # lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=config.keep_prob)
+            rnn_fw_cells = [tf.nn.rnn_cell.LSTMCell(name='fw_cell_{}'.format(i), num_units=FLAGS.hidden) for i in
+                            range(FLAGS.layers)]
+            rnn_bw_cells = [tf.nn.rnn_cell.LSTMCell(name='bw_cell_{}'.format(i), num_units=FLAGS.hidden) for i in
+                            range(FLAGS.layers)]
+        elif FLAGS.unit == 'gru':
+            rnn_fw_cells = [tf.nn.rnn_cell.GRUCell(name='fw_cell_{}'.format(i), num_units=FLAGS.hidden) for i in
+                            range(FLAGS.layers)]
+            rnn_bw_cells = [tf.nn.rnn_cell.GRUCell(name='bw_cell_{}'.format(i), num_units=FLAGS.hidden) for i in
+                            range(FLAGS.layers)]
+        else:
+            raise ValueError('Unit must be lstm or gru!')
+        if FLAGS.bi:
+            # multi-layers bidirectional RNN
+            outputs, output_states_fw, output_states_bw = tf.contrib.rnn.stack_bidirectional_dynamic_rnn(
+                rnn_fw_cells,
+                rnn_bw_cells,
+                embedding,
+                dtype=DATA_TYPE,
+                sequence_length=self._seq_len
+            )
+            self._inter_output = self.linear_layer('out', outputs, 2 * FLAGS.hidden, output_size, axes=[[2], [0]])
+        else:
+            # multi-layers RNN
+            multi_rnn = tf.contrib.rnn.MultiRNNCell(cells=rnn_fw_cells, state_is_tuple=True)
+            outputs, state = tf.nn.dynamic_rnn(
+                multi_rnn,
+                embedding,
+                dtype=DATA_TYPE,
+                sequence_length=self._seq_len
+            )
+            self._inter_output = self.linear_layer('out', outputs, FLAGS.hidden, output_size, axes=[[2], [0]])
+        self._loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self._true_output,
+                                                                            logits=self._inter_output,
+                                                                            name='loss'))
+        self._pred_output = tf.nn.sigmoid(self._inter_output)
+        if not is_training:
+            return
+
+        self._global_step = tf.Variable(0, trainable=False)
+        lr = tf.train.exponential_decay(1e-3, self._global_step, 1000, 0.5, staircase=True)
+        self._train_step = tf.train.AdamOptimizer(lr).minimize(self._loss)
+        # self._train_step = tf.train.AdamOptimizer(1e-3).minimize(self._loss)
+
+    def linear_layer(self, name, x, input_size, output_size, axes=None):
+        with tf.variable_scope(name):
+            weight = tf.get_variable(
+                name='w',
+                shape=(input_size, output_size),
+                dtype=DATA_TYPE,
+                initializer=tf.keras.initializers.glorot_uniform()
+            )
+            bias = tf.get_variable(
+                name='b',
+                shape=(output_size,),
+                dtype=DATA_TYPE,
+                initializer=tf.zeros_initializer()
+            )
+            y = tf.matmul(x, weight) if axes is None else tf.tensordot(x, weight, axes=axes)
+            y = tf.add(y, bias)
+            return y
+
+    @property
+    def input(self):
+        return self._input
+
+    @property
+    def seq_len(self):
+        return self._seq_len
+
+    @property
+    def true_output(self):
+        return self._true_output
+
+    @property
+    def pred_output(self):
+        return self._pred_output
+
+    @property
+    def global_step(self):
+        return self._global_step
+
+    @property
+    def loss(self):
+        return self._loss
+
+    @property
+    def train_step(self):
+        return self._train_step
+
+    @property
+    def inter_output(self):
+        return self._inter_output
+
+
+def main(_):
+    feature = list(range(0, 21)) + list(range(22, 67))
+    # print(feature)
+    # feature = None
+    os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu
+    ds = JSONData(FLAGS.input_file, FLAGS.start, FLAGS.end, feature=feature)
+    print('Data loaded, size={}.'.format(ds.train_size))
+    with tf.Graph().as_default(), tf.Session() as session:
+        with tf.variable_scope('model', initializer=tf.keras.initializers.glorot_uniform()):
+            model = Model(True, 66, 1)
+        with tf.variable_scope('model', reuse=True, initializer=tf.keras.initializers.glorot_uniform()):
+            model_test = Model(False, 66, 1)
+        session.run(tf.global_variables_initializer())
+        temp = '{:<5}' + 10 * ' {:<15}'
+        print(temp.format('loop', 'train_loss', 'test_loss', 'Accuracy', 'Precision', 'Recall', 'F1',
+                          'NDCG', 'Log Loss', 'MSPE', 'AUC'))
+        acc = 0
+        best_test_loss = 100
+        for iter in range(1, FLAGS.loops + 1):
+            # train
+            train_x, train_y, train_seq = ds.next_batch(FLAGS.batch)
+            train_fetches = [
+                model.train_step,
+                model.loss,
+                model.pred_output
+            ]
+            train_feed = {
+                model.input: train_x,
+                model.true_output: train_y,
+                model.seq_len: train_seq,
+                model.global_step: iter
+            }
+            _, train_loss, output = session.run(train_fetches, train_feed)
+            # in sample
+            # test_x, test_y, test_seq = ds.train_data
+            # out sample
+            test_x, test_y, test_seq = ds.test_data
+            test_fetches = [
+                model_test.loss,
+                model_test.pred_output
+            ]
+            test_feed = {
+                model_test.input: test_x,
+                model_test.true_output: test_y,
+                model_test.seq_len: test_seq
+            }
+            test_loss, output = session.run(test_fetches, test_feed)
+            # evaluation
+            true_y = []
+            pred_y = []
+            # in sample
+            # train_stkcds = []
+            # train_years = []
+            # for i, j, k, p, q in zip(output, test_y, test_seq, ds.train_stkcd, ds.train_year):
+            #     for m in range(k):
+            # train_stkcds.append(p)
+            # train_years.append(q[m])
+            # pred_y.append(i[m][0])
+            # true_y.append(j[m][0])
+            # out sample
+            test_stkcd = ds.test_stkcd
+            for i, j, k in zip(output, test_y, test_seq):
+                pred_y.append(i[k - 1][0])
+                true_y.append(j[k - 1][0])
+            acc_temp, precision_temp, recall_temp, f1_temp, auc_temp, ndcg_temp, log_temp, mspe_temp = evaluation(
+                true_y,
+                pred_y,
+                threshold=FLAGS.threshold)
+            temp = '{:<5d}' + 10 * ' {:<15.8f}'
+            print(temp.format(iter, train_loss, test_loss, acc_temp, precision_temp, recall_temp, f1_temp,
+                              ndcg_temp, log_temp, mspe_temp, auc_temp))
+            if test_loss < best_test_loss:
+                best_test_loss = test_loss
+                best_loss_loop = iter
+            if acc_temp > acc:
+                acc = acc_temp
+                best_acc_loop = iter
+                # result = [acc_temp, precision_temp, recall_temp, f1_temp, ndcg_temp, log_temp, mspe_temp, auc_temp]
+                result = [precision_temp, recall_temp, ndcg_temp, auc_temp]
+                best_pred_y = pred_y
+        print('Result({}): {}'.format(best_acc_loop, result))
+        print('Test loss({}): {}'.format(best_loss_loop, best_test_loss))
+        # save
+        with open('result/rnn/ICD_dataset_240218/{}_{}_{}_{}_{}.txt'.format(FLAGS.end + 1, FLAGS.layers, FLAGS.hidden,
+                                                                            FLAGS.keep, FLAGS.emb), 'w') as f:
+            for i, j, k in zip(test_stkcd, true_y, best_pred_y):
+                f.write('{} {} {}\n'.format(i, j, k))
+        # with open('result/rnn/insample.txt', 'w') as f:
+        #     for i, j, k, l in zip(train_stkcds, train_years, true_y, best_pred_y):
+        #         f.write('{} {} {} {}\n'.format(i, j, k, l))
+
+
+if __name__ == '__main__':
+    tf.app.run()
